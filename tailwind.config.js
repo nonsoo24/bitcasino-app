6038b5e2a66c054f2a4e6d9c5c254535b231a6b1
Daniel Chukwurah <danielchukwurah@Nonso-MacBook-Pro.local>
2022-08-10 08:00:03 +0100

    chore: modify configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#2a0a4a',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
